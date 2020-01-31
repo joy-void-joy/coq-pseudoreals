86cb2360d0ad4c8298a37ff035b60e62590e6d5f
Joy Void Joy <joy.void.joy@gmail.com>
2020-02-01 00:03:52 +0100

    theory: Add additions of games

--- src/surreal.v
+++ src/surreal.v
@@ -6,17 +6,22 @@ Inductive Game :=
 |mkGame of (Ensemble Game) & (Ensemble Game).
 Coercion ensembleg_of_game (e: Game): Ensemble Game := [:: e].
 
-Notation "{}" := (finEnsemble Game [::]).
+Notation "{}" := (finEnsemble [::]).
 Notation "{ l | r }" := (mkGame l r) (at level 0, format "'[' { l | r } ']'", l at level 99).
 Notation "{ l | }" := {l | {}} (at level 0, format "{ l | }", l at level 99).
 Notation "{ | r }" := {{} | r} (at level 0, format "{ | r }").
 Notation "{|}" := {{} | {}} (at level 0, format "{|}").
 
 Fixpoint game_of_nat n := if n is m.+1 then {(game_of_nat m) | } else {|}.
-Fixpoint negative g := let: {l | r} := g in {map negative r | map negative l}.
+Fixpoint negative g := let: {l | r} := g in {map negative r | map negative l}
+  where "- a" := (negative a).
 Definition game_of_Z z := let: res := game_of_nat (Z.abs_nat z) in if Z.geb 0 z then res else negative res.
 Coercion game_of_Z: Z >-> Game.
-Fixpoint plus (a b: Game): Game := let: ({al | ar}, {bl | br}) := (a, b) in {union (map (plus b) al) (map (plus a) bl) | union (map (plus b) ar) (map (plus a) bl)}.
-Definition x_plus_half (x: Z) := {x | x+1}.
+
+Fixpoint plus (a: Game) := fix plus_a b :=
+  let: ({al | ar}, {bl | br}) := (a, b) in
+  {union (map (plus^~b) al) (map plus_a bl) | union (map (plus^~b) ar) (map plus_a br)}
+where "a + b" := (plus a b).
+
 
 -
